0ef8b887dfdf1eeb833ae4a308ebf83b5b66cdf8
Eric Stofferahn <Eric.Stofferahn@noaa.gov>
2020-02-14 11:31:16 -0500

    Happy Valentine's Day!!

---
+++ valentine.f90
@@ -0,0 +1,32 @@
+program valentine
+implicit none
+
+real, parameter  :: howMuchFortranYouCanHandle=0.5 ! Move this parameter
+                                                   ! closer to 1 if you
+                                                   ! despise Fortran, or
+                                                   ! move it towards 0 if
+                                                   ! you like Fortran
+
+print *, "Will Fortran be my Valentine?"
+print *, chalkHeart(howMuchFortranYouCanHandle)
+
+contains
+    
+!> @brief Listen to your heart
+!! Allow the chalk heart to determine whether Fortran will be your
+!! Valentine this year.
+function chalkHeart(thresholdFortran)
+real, intent(in) :: thresholdFortran
+
+character(len=4) :: chalkHeart
+real             :: loveFortran
+
+call RANDOM_SEED()
+call RANDOM_NUMBER(loveFortran)
+chalkHeart = "NO!!"
+if (loveFortran .gt. thresholdFortran) then
+  chalkHeart = "YES!"
+endif
+end function chalkHeart
+
+end program valentine
