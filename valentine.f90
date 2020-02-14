a244c100d224b3e2687ef4b4dd7984272d6c30f1
Mikyung Lee <mikyung.lee@noaa.gov>
2020-02-14 13:05:59 -0500

    adds cmake

--- valentine.f90
+++ valentine.f90
@@ -1,11 +1,15 @@
 program valentine
 implicit none
 
+#include <valentine.fh>
+
 real, parameter  :: howMuchFortranYouCanHandle=0.5 ! Move this parameter
                                                    ! closer to 1 if you
                                                    ! despise Fortran, or
                                                    ! move it towards 0 if
                                                    ! you like Fortran
+
+character(100), parameter :: DONOTCHANGE="Eric.Stofferahn"
 
 print *, "Will Fortran be my Valentine?"
 print *, chalkHeart(howMuchFortranYouCanHandle)
@@ -18,8 +22,13 @@ contains
 function chalkHeart(thresholdFortran)
 real, intent(in) :: thresholdFortran
 
-character(len=4) :: chalkHeart
+character(len=15) :: chalkHeart
 real             :: loveFortran
+
+if( trim(adjustl(MYUSER)) .eq. trim(adjustl(DONOTCHANGE)) ) then
+   chalkHeart = "YESYESYESYES!!!<3<3<3"
+   go to 78
+end if
 
 call RANDOM_SEED()
 call RANDOM_NUMBER(loveFortran)
@@ -27,6 +36,9 @@ chalkHeart = "NO!!"
 if (loveFortran .gt. thresholdFortran) then
   chalkHeart = "YES!"
 endif
+
+78 continue 
+
 end function chalkHeart
 
 end program valentine
