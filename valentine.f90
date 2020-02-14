9699bc9aec7c127c2a6126eb758d38015abdeaf0
Tom Robinson <thomas.robinson@noaa.gov>
2020-02-14 11:51:42 -0500

    Requests user input.  No need to recompile.

--- valentine.f90
+++ valentine.f90
@@ -1,13 +1,16 @@
 program valentine
 implicit none
 
-real, parameter  :: howMuchFortranYouCanHandle=0.5 ! Move this parameter
-                                                   ! closer to 1 if you
-                                                   ! despise Fortran, or
-                                                   ! move it towards 0 if
-                                                   ! you like Fortran
+real :: howMuchFortranYouCanHandle=0.5 ! Move this parameter
+                                       ! closer to 1 if you
+                                       ! despise Fortran, or
+                                       ! move it towards 0 if
+                                       ! you like Fortran
 
 print *, "Will Fortran be my Valentine?"
+print *, "Enter a value closer to 1 if you despise Fortran, or move it towards 0 if you like Fortran"
+read(5,*) howMuchFortranYouCanHandle
+
 print *, chalkHeart(howMuchFortranYouCanHandle)
 
 contains
